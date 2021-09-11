2032314dccff188941723e51189c36ce8a5157a3
Jaricapa-holberton <2241@holbertonschool.com>
2021-09-11 17:28:18 -0400

    task 1 v1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+First Flask web application, this app is
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+# instance the class flask
+app = Flask(__name__)
+
+# assing to Flask what URL trigger our function
+# route for '/'
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+# route for '/hbnb'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display “HBNB”"""
+    return 'HBNB'
+
+# app_run modify where execute the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
