8fe768b4d9c2800970cd6f05c1f8d3f80a0d80a0
Jaricapa-holberton <2241@holbertonschool.com>
2021-09-11 16:37:24 -0500

    task 2 v1

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+First Flask web application, this app is
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+# instance the class flask
+app = Flask(__name__)
+
+
+# assing to Flask what URL trigger our function
+# route for '/'
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+# route for '/hbnb'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display “HBNB”"""
+    return 'HBNB'
+
+# route for '/c/<text>'
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """Display “C ” followed by <text> variable"""
+    C_string = "C {}".format(text.replace("_", " "))
+    return C_string
+
+# app_run modify where execute the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
