32f9e841b4919911b309a0aa9374d4aef4abd8c3
Jaricapa-holberton <2241@holbertonschool.com>
2021-09-11 14:37:48 -0400

    update task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+First Flask web application, this app is
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+# instance the class flask
+app = Flask(__name__)
+
+# assing to Flask what URL trigger our function
+@app.route('/', strict_slashes=False)
+# create app for run
+def hello():
+    """Display “Hello HBNB!”"""
+    return 'Hello HBNB!'
+
+# app_run modify where execute the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
